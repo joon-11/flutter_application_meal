47a40269be75da00c10b6f642cb78ba1a7ede3e6
KJ <rlawns0604122@gmail.com>
2023-05-23 18:46:19 +0900

    first commit

---
+++ lib/neis_api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NeisApi {
+  Future<List<dynamic>> getMeal(
+      {required String fromDate, required String toDate}) async {
+    String site =
+        'https://open.neis.go.kr/hub/mealServiceDietInfo?2559e99bcbd04fd89364a4f4235abd15&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7551010&MLSV_FROM_YMD=$fromDate&MLSV_TO_YMD=$toDate';
+    var response = await http.get(Uri.parse(site));
+    if (response.statusCode == 200) {
+      var meals = jsonDecode(response.body)['mealServiceDietInfo'][1]['row']
+          as List<dynamic>;
+      return meals;
+    } else {
+      return [];
+    }
+  }
+}
